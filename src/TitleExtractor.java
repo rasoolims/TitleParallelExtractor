1c5078ceffa104a04bfc656cdcd56742125b4793
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-07-31 14:22:34 -0400

    Extract comparable sentences

--- src/TitleExtractor.java
+++ src/TitleExtractor.java
@@ -5,6 +5,12 @@ import java.io.FileWriter;
 import java.util.HashMap;
 
 public class TitleExtractor {
+    public static String[] extractSentences(String input) {
+        String[] sentences = input.trim().split("</s>");
+        sentences[0].substring(sentences[0].indexOf(">") + 1).trim();
+        return sentences;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader titleReader = new BufferedReader(new FileReader(args[0]));
         HashMap<String, String> titleDict = new HashMap<>();
@@ -26,8 +32,8 @@ public class TitleExtractor {
         String wikiLine = null;
         int lineNum = 0;
         while ((wikiLine = smallWikiReader.readLine()) != null) {
-            String[] sentences = wikiLine.trim().split("</s>");
-            String title = sentences[0].substring(sentences[0].indexOf(">") + 1).trim();
+            String[] sentences = extractSentences(wikiLine);
+            String title = sentences[0];
             if (titleDict.containsKey(title)) {
                 wikiConent.put(title, wikiLine.trim());
             }
@@ -40,23 +46,49 @@ public class TitleExtractor {
         System.out.println("Reading " + args[2]);
         BufferedReader bigWikiReader = new BufferedReader(new FileReader(args[2]));
         BufferedWriter parWriter = new BufferedWriter(new FileWriter(args[3]));
+        boolean extract = args.length > 3 && args[3].equals("comp");
         lineNum = 0;
         int parallel = 0;
+        int comparabale = 0;
         while ((wikiLine = bigWikiReader.readLine()) != null) {
-            String[] sentences = wikiLine.trim().split("</s>");
-            String title = sentences[0].substring(sentences[0].indexOf(">") + 1).trim();
+            String[] sentences = extractSentences(wikiLine);
+            String title = sentences[0];
             if (revTitleDict.containsKey(title) && wikiConent.containsKey(revTitleDict.get(title))) {
-                parWriter.write(wikiConent.get(revTitleDict.get(title)));
-                parWriter.write(wikiLine.trim());
-                parWriter.write("\t");
-                parWriter.write("\n");
+                if (!extract) {
+                    parWriter.write(wikiConent.get(revTitleDict.get(title)));
+                    parWriter.write(wikiLine.trim());
+                    parWriter.write("\t");
+                    parWriter.write("\n");
+                } else {
+                    String[] srcSentences = extractSentences(wikiConent.get(revTitleDict.get(title)));
+                    for (int r = 0; r < srcSentences.length; r++) {
+                        String refSen = srcSentences[r];
+                        float refRegion = ((float) r) / srcSentences.length;
+                        int refLen = refSen.split(" ").length;
+
+                        int doc_start_range = (int) Math.floor(Math.max(0, refRegion - 0.1) * sentences.length);
+                        int doc_end_range = 1; // Title by title alignment
+                        if (r > 0)
+                            doc_end_range = (int) Math.ceil(Math.min(1, refRegion + 0.1) * sentences.length);
+
+                        for (int s = doc_start_range; s < doc_end_range; s++) {
+                            String sen = sentences[s];
+
+                            int senLen = sen.split(" ").length;
+                            double proportion = ((double) refLen) / senLen;
+                            if (Math.abs(refLen - senLen) <= 3 || (0.9 <= proportion && proportion <= 1.1)) {
+                                parWriter.write(refSen + " ||| " + sen + "\n");
+                                comparabale++;
+                            }
+                        }
+                    }
+                }
                 parallel++;
             }
             lineNum++;
             if (lineNum % 1000 == 0)
-                System.out.print(lineNum + " -> " + parallel + "\r");
+                System.out.print(lineNum + " -> " + comparabale + " / " + parallel + "\r");
         }
         System.out.println("\nFinished: " + parallel);
-
     }
 }
